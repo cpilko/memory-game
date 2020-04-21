bdc3dee1c2f257345d768c24379e5cf871f259ae
Chris <chris.pilko@gmail.com>
2020-04-21 09:48:41 -0400

    Changes to make the program more readable

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const board = document.getElementById('game-board');
 const max_cards = 12;
 const card_faces = ['d', 'e', 'f', 'h', 'i', 'j'];
-const card_colors = ['#ffeaa7', '#00b894', '#e17055', '#e84393', '#fd79a8', '#6c5ce7'];
+const color_classes = ['sour-lemon', 'mint-leaf', 'pink-glamour', 'prunus-avium', 'pico-pink', 'exodus-fruit'];
 const card_back = '?';
 
 let has_flipped_card = false;
@@ -14,18 +14,18 @@ let matched_cards = 0;
     let j = Math.floor(i/2);
     let rand = Math.floor(Math.random() * max_cards);
     let card = document.createElement('div');
-    card.setAttribute('class', 'card');
-    card.setAttribute('data-face', card_faces[j]);
+    card.classList.add('card');
+    card.dataset.face = card_faces[j];
     card.style.order = rand;
     card.addEventListener('click', flipCard);
     board.append(card);
     let front = document.createElement('div');
-    front.setAttribute('class', 'front-face');
-    front.style.color = card_colors[j];
+    front.classList.add('front-face');
+    front.classList.add(color_classes[j]);
     front.innerHTML = card_faces[j];
     card.append(front);
     let back = document.createElement('div');
-    back.setAttribute('class', 'back-face');
+    back.classList.add('back-face');
     back.innerHTML = card_back;
     card.append(back);
   }
@@ -48,7 +48,7 @@ function flipCard() {
 
 function checkForMatch () {
   if (first_card.dataset.face === second_card.dataset.face) {
-    disableCards();
+    matchedCards();
     matched_cards = matched_cards + 2;
     if (matched_cards == max_cards) {
       setTimeout(() => {
@@ -60,7 +60,7 @@ function checkForMatch () {
   unflipCards();
 }
 
-function disableCards() {
+function matchedCards() {
   first_card.removeEventListener('click', flipCard);
   second_card.removeEventListener('click', flipCard);
   setTimeout(() => {
